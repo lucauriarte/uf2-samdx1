4d90e71771a5b51929820123a457be1694859f90
Luca Uriarte <42620882+lucauriarte@users.noreply.github.com>
2022-10-13 12:45:57 -0300

    Update board config to use exp. payload UART

--- boards/metro_m4/board_config.h
+++ boards/metro_m4/board_config.h
@@ -1,9 +1,9 @@
 #ifndef BOARD_CONFIG_H
 #define BOARD_CONFIG_H
 
-#define VENDOR_NAME "Adafruit Industries"
-#define PRODUCT_NAME "Metro M4 Express"
-#define VOLUME_LABEL "METROM4BOOT"
+#define VENDOR_NAME "Innova Space"
+#define PRODUCT_NAME "MDQSAT-1 OBDH"
+#define VOLUME_LABEL "OBDH BOOT"
 #define INDEX_URL "http://adafru.it/3382"
 #define BOARD_ID "SAMD51J19A-Metro-v0"
 
@@ -17,15 +17,15 @@
 #define BOARD_NEOPIXEL_PIN PIN_PB22
 #define BOARD_NEOPIXEL_COUNT 1
 
-#define BOOT_USART_MODULE                 SERCOM3
+#define BOOT_USART_MODULE                 SERCOM1
 #define BOOT_USART_MASK                   APBAMASK
-#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBBMASK_SERCOM3
+#define BOOT_USART_BUS_CLOCK_INDEX        MCLK_APBAMASK_SERCOM1
 #define BOOT_USART_PAD_SETTINGS           UART_RX_PAD1_TX_PAD0
 #define BOOT_USART_PAD3                   PINMUX_UNUSED
 #define BOOT_USART_PAD2                   PINMUX_UNUSED
-#define BOOT_USART_PAD1                   PINMUX_PA22C_SERCOM3_PAD0
-#define BOOT_USART_PAD0                   PINMUX_PA23C_SERCOM3_PAD1
-#define BOOT_GCLK_ID_CORE                 SERCOM3_GCLK_ID_CORE
-#define BOOT_GCLK_ID_SLOW                 SERCOM3_GCLK_ID_SLOW
+#define BOOT_USART_PAD1                   PINMUX_PA17C_SERCOM1_PAD1
+#define BOOT_USART_PAD0                   PINMUX_PA16C_SERCOM1_PAD0
+#define BOOT_GCLK_ID_CORE                 SERCOM1_GCLK_ID_CORE
+#define BOOT_GCLK_ID_SLOW                 SERCOM1_GCLK_ID_SLOW
 
 #endif
